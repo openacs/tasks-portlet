fdb43ebb96a09c68f665a256cbfc55a3213d1d24
Malte Sussdorff <openacs@sussdorff.de>
2005-09-29 13:40:23 +0000

    More bug fixing related to the confusiton between contact_id, party_id and user_id

--- www/tasks-portlet.adp
+++ www/tasks-portlet.adp
@@ -1,3 +1,3 @@
 <if @config.shaded_p@ false>
-   <include src="../lib/tasks" contact_id=@user_id@>
+   <include src="/packages/tasks/lib/tasks" party_id=@contact_id@>
 </if>
--- www/tasks-portlet.tcl
+++ www/tasks-portlet.tcl
@@ -6,4 +6,15 @@ ad_page_contract {
 }
 
 array set config $cf
+
+# Integration with contacts
+set community_id [dotlrn_community::get_community_id_from_url]
+set organization_id [lindex [application_data_link::get_linked -from_object_id $community_id -to_object_type "organization"] 0]
+
+if {[exists_and_not_null organization_id]} {
+    set contact_id $organization_id
+} else {
+    set contact_id ""
+}
+
 set user_id [ad_conn user_id]
